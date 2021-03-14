521f7dc2b71426aa22bc1d93d94337ee983f398e
John Thompson <john@springframework.guru>
2021-03-14 13:29:02 -0400

    adding netty logging

--- src/main/java/guru/springframework/reactivebeerclient/config/WebClientConfig.java
+++ src/main/java/guru/springframework/reactivebeerclient/config/WebClientConfig.java
@@ -1,8 +1,12 @@
 package guru.springframework.reactivebeerclient.config;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 /**
  * Created by jt on 3/13/21.
@@ -12,6 +16,11 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(){
-        return WebClient.builder().baseUrl(WebClientProperties.BASE_URL).build();
+        return WebClient.builder()
+                .baseUrl(WebClientProperties.BASE_URL)
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                    .wiretap("reactor.netty.client.HttpClient", LogLevel.DEBUG,
+                            AdvancedByteBufFormat.TEXTUAL)))
+                .build();
     }
 }
